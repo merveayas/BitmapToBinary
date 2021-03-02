ac2566471c5eea38764b000cc741aa815a1115cb
merveayas <mervee.ayas@gmail.com>
2021-03-02 03:58:06 +0300

    Initial commit

---
+++ BitmapToBinary/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace BitmapToBinary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureButton = new System.Windows.Forms.Button();
+            this.convertButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(69, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureButton
+            // 
+            this.pictureButton.Location = new System.Drawing.Point(351, 314);
+            this.pictureButton.Name = "pictureButton";
+            this.pictureButton.Size = new System.Drawing.Size(155, 37);
+            this.pictureButton.TabIndex = 3;
+            this.pictureButton.Text = "upload picture";
+            this.pictureButton.UseVisualStyleBackColor = true;
+            this.pictureButton.Click += new System.EventHandler(this.pictureButton_Click);
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(351, 374);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(154, 36);
+            this.convertButton.TabIndex = 4;
+            this.convertButton.Text = "convert to binary";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 450);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.pictureButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button pictureButton;
+        private System.Windows.Forms.Button convertButton;
+    }
+}
+
---
+++ BitmapToBinary/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BitmapToBinary
+{
+    public partial class Form1 : Form
+    {
+        Bitmap img;
+        StreamWriter textFile;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Title = "Images";
+            openFileDialog1.Filter = "All images|*.bmp; *.jpg;";
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName.ToString() != "")
+            {
+                pictureBox1.ImageLocation = openFileDialog1.FileName.ToString();
+                img = new Bitmap(openFileDialog1.FileName.ToString());
+            }
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            textFile = new StreamWriter("C:\\Users\\Merve Ayas\\Desktop\\binary_image.txt");
+            string text = "";
+            for (int i = 0; i < img.Height; i++)
+            {
+                for (int j = 0; j < img.Width; j++)
+                {
+                    if (img.GetPixel(j, i).R.ToString() == "255"
+                        && img.GetPixel(j, i).G.ToString() == "255" 
+                        && img.GetPixel(j, i).B.ToString() == "255")
+                    {
+                        text = text + "0";
+                    }
+                    else
+                    {
+                        text = text + "1";
+                    }
+                    
+                }
+               
+                    text = text + "\r\n";
+                
+               
+            }
+            textFile.WriteLine(text);
+            textFile.Close();
+        }
+    }
+}
